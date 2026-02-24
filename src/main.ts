2f42415abd0a1393c6da91b5869ab17cfe617c37
psakho922 <psakho922@gmail.com>
2026-02-24 02:20:23 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // IMPORTANT pour Render
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 10000);
 }
 bootstrap();
