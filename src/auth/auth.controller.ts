833b5146c68ae13d274e735068c345e77df1cf3a
psakho922 <psakho922@gmail.com>
2026-02-25 23:10:55 +0000

    Create auth.controller.ts

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+@Controller('auth')
+export class AuthController {
+  @Post('register')
+  async register(@Body() body: any) {
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: hashedPassword,
+        name: body.name,
+      },
+    });
+
+    return { message: 'User created', user };
+  }
+}
