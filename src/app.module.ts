607fbd10645b7edce25118155a37366f9e943cad
psakho922 <psakho922@gmail.com>
2026-02-26 22:56:23 +0000

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ProductsModule],
 })
 export class AppModule {}
