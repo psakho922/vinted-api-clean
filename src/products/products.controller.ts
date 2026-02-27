d01b50ada770f14cc34d2ae8bb8e03137bd124b2
psakho922 <psakho922@gmail.com>
2026-02-27 00:24:09 +0000

    Update products.controller.ts

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,16 +1,32 @@
-@Post()
-async create(@Body() body: any) {
-  const product = await prisma.product.create({
-    data: {
-      title: body.title,
-      price: parseFloat(body.price),
-      image: body.image,
-      userId: body.userId,
-    },
-  });
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
-  return product;
+const prisma = new PrismaClient();
+
+@Controller('products')
+export class ProductsController {
+
+  @Post()
+  async create(@Body() body: any) {
+    const product = await prisma.product.create({
+      data: {
+        title: body.title,
+        price: parseFloat(body.price),
+        image: body.image,
+        userId: body.userId,
+      },
+    });
+
+    return product;
+  }
+
+  @Get()
+  async findAll() {
+    return prisma.product.findMany({
+      include: {
+        user: true,
+      },
+    });
+  }
 }
-
-
 
