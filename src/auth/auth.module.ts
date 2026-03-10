f6f41ea52b32b33c14879a9f399a3e6950c7f03d
psakho922 <psakho922@gmail.com>
2026-03-10 23:06:45 +0000

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { AuthController } from './auth.controller';
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "./auth.controller";
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'SUPER_SECRET_KEY_CHANGE_ME',
-      signOptions: { expiresIn: '1d' },
-    }),
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: "30d"
+      }
+    })
   ],
   controllers: [AuthController],
+  exports: [JwtModule]
 })
 export class AuthModule {}
