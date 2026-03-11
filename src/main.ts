aee1088b40de39ed2ee953f3bdc0b9df363c0aae
psakho922 <psakho922@gmail.com>
2026-03-11 22:37:49 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+}
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true
-  });
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors();
+
+  await app.listen(3001);
 
   console.log("Backend running");
 
 }
 
-bootstrap();
+bootstrap()
